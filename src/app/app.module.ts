22e667dcbe36a9911bec7e3a312ef0173b43caf0
Piyush <piyushpatel.love6@gmail.com>
2017-12-21 16:11:52 +0530

    Material theme added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,18 @@ import {HttpclientService} from "./services/httpclient.service";
 import {AuthInterceptor} from "./intercepter/auth-interceptor";
 import {AuthService} from "./services/auth.service";
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import { HomeComponent } from './home/home.component';
+import {HomeComponent} from "./home/home.component";
 import {Routing} from "./routes";
 import {AuthGuard} from "./auth.guard";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatFab,
+  MatIcon,
+  MatMiniFab,
+  MatRaisedButtonCssMatStyler
+} from "@angular/material";
 
 @NgModule({
   declarations: [
@@ -16,9 +25,13 @@ import {AuthGuard} from "./auth.guard";
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
-    Routing
+    Routing,
+    MatButtonModule,
+    MatCheckboxModule,
   ],
+  exports: [MatButtonModule, MatCheckboxModule],
   providers: [
     HttpclientService,
     AuthGuard,
